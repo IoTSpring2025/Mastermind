7e0130e065ead94f9ab134e2fa88bd091fd07ecb
jkrue242 <josephkrueger242@gmail.com>
2025-02-15 17:11:48 -0600

    Image streaming compatibility

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,8 @@
 from roboflow import Roboflow
 import os
-
+import tempfile
+from PIL import Image
+import io 
 
 class Model():
     def __init__(self, version=4, confidence=33, overlap=15):
@@ -11,22 +13,15 @@ class Model():
         self.confidence = confidence
         self.overlap = overlap
     
-    def read_hand(image_path):
-        """
-        TODO: don't use path, differentiate from board
-        """
-        prediction = self.model.predict(image_path, confidence=self.confidence, overlap=self.overlap)
-        output = {}
-        for pred in prediction.json()['predictions']:
-            output[pred["class"]] = f"{pred['confidence']*100:.2f}%"
-        return output
-
-    def read_board(image_path):
-        """
-        TODO: don't use path, differentiate from hand
-        """
-        prediction = self.model.predict(image_path, confidence=self.confidence, overlap=self.overlap)
-        output = {}
-        for pred in prediction.json()['predictions']:
-            output[pred["class"]] = f"{pred['confidence']*100:.2f}%"
-        return output
+    def read_hand(self, image_bytes):
+        try:
+            with tempfile.NamedTemporaryFile(suffix=".jpg") as temp:
+                image = Image.open(io.BytesIO(image_bytes))
+                image.save(temp.name, format="JPEG")
+                prediction = self.model.predict(temp.name, confidence=self.confidence, overlap=self.overlap)
+            output = {}
+            for pred in prediction.json()['predictions']:
+                output[pred["class"]] = f"{pred['confidence']*100:.2f}%"
+            return output
+        except Exception as e:
+            return f"Error reading hand from bytes: {e}"
