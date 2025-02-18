ef937b44f1a8cc33b0eede6485fb34f8d4bc0622
Joseph <98860676+jkrue242@users.noreply.github.com>
2025-02-17 18:32:46 -0600

    Bump model (#13)

--- ml/download_dataset.py
+++
@@ -1,7 +0,0 @@
-from roboflow import Roboflow
-import os
-
-rf = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
-project = rf.workspace(os.environ.get("ROBOFLOW_WORKSPACE")).project(os.environ.get("ROBOFLOW_PROJECT"))
-version = project.version(4)
-dataset = version.download("yolov8")
